3809e98c27d347ab6bbb075a6b6bae8b208bc0d7
Michael <ming01zhou@gmail.com>
2020-09-27 11:23:10 -0700

    mod component name

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
+/*********************      component  gnav-bar       *********************************************************************/
+Vue.component('gnav-bar', {
+  props: ['nlink'] ,
+  template: `<div id="gnav"><ul id="nav" > 
+               <li v-for="item in links"> <a v-bind:href="item.ul">{{item.title}}</a>
+               <ul >   <li v-for="lk in item.sub"> <a v-bind:href="lk.url">{{lk.name}}</a> </li>  </ul>
+                </li></ul></div> `  ,
+  data() {
+    return { 
+        checked: false, 
+        links: [
+                      { title:'Home', ul:'./',sub:[{name:'Book Keeping', url:'#/accounting'} ,
+                                                   {name:'Income Statement', url:'#/income'} ,
+                                                   {name:'Balance Sheet', url:'#/abalance'} ,
+                                                   {name:'Invoice Entry', url:'#/accInvoice'}]},
+                      { title:'Products', ul:'#/products', sub:[{name:'Product List', url:'#'},{name:'Pre-Order', url:'#/preOrder'},{name:'Gecon Admin 2020', url:'../me2020'} ]} ,
+                      { title:'GoPHP', ul:'./src/php/getPrice.php', sub:[{name:'Price Update PERL', url:'./src/perl/two.pl'} ,
+                                                                         {name:'link 3', url:'#/accInvoice/1'} ,
+                                                                         {name:'link 4', url:'#/accInvoice/2'}]} ,
+                      { title:'Portfolio', ul:'#/portfolio', sub:[{name:'Portfolio Total', url:'#/portfo'} ,
+                                                                  {name:'Portfolio Zhou',  url:'#/portfo/1'} ,
+                                                                  {name:'Portfolio Fan',   url:'#/portfo/2'} ,
+                                                                  {name:'Dividend Income',  url:'#/portfo/3'} ,
+                                                                  {name:'TermDep', url:'#/termdeposit'} ,{name:'Activity', url:'#/portfolio/2'}]} ,
+                      { title:'Login', ul:'#/login',sub:[{name:'Register', url:'#/register'} ,{name:'Gecon Admin Beta', url:'./pre_index.php'}]} 
+                    ]  ,
+        title: 'Check me' 
+        
+    }
+  }
+})
 /*******************************************************************************************************************/
 /***********                                        New a Vue #App         Sept 27 , 2020                    *******/
 /*******************************************************************************************************************/
@@ -5,7 +36,7 @@ var app = new Vue({
   el: '#app' ,
   data: {
       message: 'Hello New app Vue !!' ,
-      currentView:'Author',
+      currentView:'gnav-bar',
       logn:true,
       jsdd:'',
       amp:'',
