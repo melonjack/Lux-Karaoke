fbf1b31795614f9a75c0c6a89d19c7dc2d925492
Michael <ming01zhou@gmail.com>
2020-10-12 16:33:28 -0700

    sub component

--- test/dxvgi/webaccount.js
+++ test/dxvgi/webaccount.js
@@ -26,4 +26,43 @@ mounted: function(){
 console.log("Hello 12!")
 }
 }) 
+/******************************************************************************************************************/
+/***                                    Component atrans b  for accounting     October   2020     Michael       ***/
+/******************************************************************************************************************/
+var  atransb  = Vue.component('atransb' , 
+ { 
+  props: ['subd','pbc'] ,
+  template: ` <div class="fd1"> <br>module <br>
+    {{ tno}}
+          <table  class="account" >
+                    <caption> General Ledger <br>Acoount: {{subd.gt}}</caption>
+                      <tr><th>Trans No.</th><th>Date</th><th>Referance</th<th>Debit</th><th>Credit</th><th>Comment</th><th>Balance</th></tr>
+                      <tr v-for= "(trn, index ) in subd.oneledg"><td>{{trn.trans_no}} </td><td>{{trn.date}}</td><td>{{trn.referance}}</td><td>{{trn.debit}}</td><td>{{trn.credit}}</td>
+                        <td>{{trn.comment}}</td><td>{{trn.balance}}</td></tr>
+                          </table>
+    
+                    </div>  ` ,
+  data() {
+      return {  
+              tm:  false,
+              tno:243,
+              acclink:'',
+              data:'',
+              dt_s:'',
+              dt_e:'',
+              b: 54 
+                }
+   },
+mounted: function(){
+      console.log(this.$route.params.pid)
+      this.b = this.$route.params.pid 
+      let url = 'http://www.gecontech.com/magento/mgc20/src/php/api_db_2020.php?action=onetrans&tt_no=' + this.tno
+      axios.get(url).then(function(response){
+                app.jsdd = response.data ;   
+                console.log(app.jsdd) ;  }) .catch(function(error){ console.log(error) ;  }) ;
+      
+     }
+
+}) 
+/****************************************************************************************************************/
 /*******************************************************************************************************************/
