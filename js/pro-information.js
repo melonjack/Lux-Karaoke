3c9b9c03b76834bd1a086ec5d7939139a3186be3
Michael <ming01zhou@gmail.com>
2020-11-09 16:58:45 -0800

    Vue

--- js/pro-information.js
+++ js/pro-information.js
@@ -108,3 +108,6 @@ var  list2 = Vue.component('list2' ,
     }
   } 
 })
+Vue.component('component-a', { template: `<div class="listitem">  Hello World !</div> ` })
+Vue.component('component-b', { template: `<div class="listitem">  Hello World !</div> ` })
+Vue.component('component-c', { template: `<div class="listitem">  Hello World !</div> ` })
