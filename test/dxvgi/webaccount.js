d557f0ba24fa73c45dca85d34f6a7f8ff9a160f2
Michael <ming01zhou@gmail.com>
2020-12-06 17:10:16 -0800

    tref

--- test/dxvgi/webaccount.js
+++ test/dxvgi/webaccount.js
@@ -48,12 +48,13 @@ var  atransb  = Vue.component('atransb' ,
                           </table>
                <button class="btn btn-primary btn-sm" v-on:click="getrecord(subd)">Re-Post</button>
               <div class="container" v-if="ten">
-              Account No: Loan (From Shareholder) Reference No: General Entries Entry Date: <input type="date" class="form1" v-model="dt0"> Customer Name:GTL Solutions Inc.<br>
+                   Account No: Loan (From Shareholder) Reference No:  <input type="date" class="form1" v-model="dt0">
+                  Entry Date: <input type="text" class="form1" v-model="tref"> Customer Name:GTL Solutions Inc.<br>
 
                     <table class="account" v-if="ten">
-                    <tr><th>Trans No.</th><th>Date</th><th>Account no</th><th>Account Name</th><th>Debit</th><th>Credit</th><th>Referance</th><th>Comment</th></tr>
+                    <tr><th>Account Name</th><th>Debit</th><th>Credit</th><th>Comment</th></tr>
                     <tr  v-for="(trn, index ) in data"> <td><input type="text" class="" v-model="trn.acc_nm"></td>
-                    <td><input type="text" class="" v-model="trn.debit"></td><input type="text" class="" v-model="trn.crdit"></td><td><input type="text" class="" v-model="trn.comment"></td>
+                    <td><input type="text" class="" v-model="trn.debit"></td><input type="text" class="" v-model="trn.credit"></td><td><input type="text" class="" v-model="trn.comment"></td>
                      </tr></table>
                     <button type="submit" class="btn btn-success">Update</button> <button type="submit" class="btn btn-info">Submit</button>
                    
@@ -70,6 +71,7 @@ var  atransb  = Vue.component('atransb' ,
               acclink: '33',
               data: 'L',
               dt0: "2020-10-10",
+              tref: "generl Entry",
               dt_s: '',
               dt_e: '',
               b: 54, 
@@ -97,7 +99,8 @@ var  atransb  = Vue.component('atransb' ,
          this.tm = false
          this.ten = true
          this.data = mx.oneledg
-         this.dt0 = this.data.date        
+         this.dt0 = this.data.date
+         this.tref = this.data.referance        
          console.log(this.data)
          return "yes"
      },
